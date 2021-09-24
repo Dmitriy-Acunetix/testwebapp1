b2899b34c4b9175ce68211e2d1228f830ef3f387
Dmitriy-Acunetix <dmitriy.marchenko@acunetix.com>
2021-09-24 09:22:13 +0200

    Second code commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ http.createServer(function (req, res) {
 
   res.write('<html><head></head><body>');
 
-  res.write('Welcome to the Test Web Application!'+'<br/>');
+  res.write('Welcome to the Test Web Application 09:41!'+'<br/>');
 
   res.write('===================================='+'<br/>');
 
